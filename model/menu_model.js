cb5975e8c7baec7862aae8d6e3ab0a0b1b80bf90
Jelle De Loecker <jelle@kipdola.be>
2015-08-06 11:06:09 +0200

    Fixes

--- model/menu_model.js
+++ model/menu_model.js
@@ -10,36 +10,52 @@ var MenuItemTypes = alchemy.shared('Menu.itemTypes');
  * @version  1.0.0
  */
 var Menu = Function.inherits('Model', function MenuModel(options) {
-
-	var chimera,
-	    list,
-	    edit,
-		view;
-
 	MenuModel.super.call(this, options);
+});
 
-	// Create the chimera behaviour
-	chimera = this.addBehaviour('chimera');
+/**
+ * Constitute the class wide schema
+ *
+ * @author   Jelle De Loecker <jelle@develry.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ */
+Menu.constitute(function constitute() {
+	this.addField('name', 'String');
+	this.hasMany('MenuPiece');
+});
+
+/**
+ * Configure chimera for this model
+ *
+ * @author   Jelle De Loecker <jelle@develry.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ */
+Menu.constitute(function chimeraConfig() {
+
+	var list,
+	    edit,
+	    view;
+
+	if (!this.chimera) {
+		return;
+	}
 
 	// Get the list group
-	list = chimera.getActionFields('list');
+	list = this.chimera.getActionFields('list');
 
 	list.addField('name');
 
 	// Get the edit group
-	edit = chimera.getActionFields('edit');
+	edit = this.chimera.getActionFields('edit');
 
 	edit.addField('name');
 
 	// Get the view group
-	view = chimera.getActionFields('view');
+	view = this.chimera.getActionFields('view');
 
 	view.addField('name');
-});
-
-Menu.constitute(function constitute() {
-	this.addField('name', 'String');
-	this.hasMany('MenuPiece');
 });
 
 /**
