e493d400aa8caa3b8ad70b70f1e17bcc30fd9aa2
Jelle De Loecker <jelle@elevenways.be>
2022-12-09 17:31:34 +0100

    Add the `Href` link class

---
+++ helper/link_types/href_link.js
@@ -0,0 +1,47 @@
+/**
+ * The Href link class
+ *
+ * @constructor
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.6.2
+ * @version  0.6.2
+ */
+const HrefLink = Function.inherits('Alchemy.Menu.Link', 'Href');
+
+/**
+ * Set the schema
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.6.2
+ * @version  0.6.2
+ */
+HrefLink.constitute(function setSchema() {
+
+	this.schema.addField('href', 'String', {
+		description: 'The actual href to use',
+	});
+
+	this.schema.addField('breadcrumb', 'String', {
+		description: 'The breadcrumb in case it\'s a local link',
+	});
+
+});
+
+/**
+ * Populate a link widget
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.6.2
+ * @version  0.6.2
+ *
+ * @param    {HTMLAnchorElement}   anchor
+ * @param    {Alchemy.Widget.Link} widget
+ */
+HrefLink.setMethod(function populateWidget(anchor, widget) {
+	anchor.setAttribute('href', this.settings.href || '');
+
+	if (this.settings.breadcrumb) {
+		anchor.setAttribute('data-breadcrumb', this.settings.breadcrumb);
+	}
+});
