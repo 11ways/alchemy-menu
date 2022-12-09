737d5c3c4c578d676a994da3645c19c26dd3e84a
Jelle De Loecker <jelle@elevenways.be>
2022-12-09 17:31:46 +0100

    Add the incomplete `Route` link class

---
+++ helper/link_types/route_link.js
@@ -0,0 +1,76 @@
+/**
+ * The Route link class
+ *
+ * @constructor
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.6.2
+ * @version  0.6.2
+ */
+const RouteLink = Function.inherits('Alchemy.Menu.Link', 'Route');
+
+/**
+ * Set the schema
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.6.2
+ * @version  0.6.2
+ */
+RouteLink.constitute(function setSchema() {
+
+	let routes;
+
+	if (Blast.isNode) {
+		routes = Router.routes.getDict();
+	} else {
+
+		routes = {};
+
+		// for (let root in hawkejs.scene.exposed.routes) {
+		// 	let section = hawkejs.scene.exposed.routes[root];
+
+		// 	for (let key in section) {
+		// 		routes[key] = section[key];
+		// 	}
+		// }
+	}
+
+	this.schema.addField('route', 'Enum', {
+		values                 : routes,
+		widget_config_editable : true,
+	});
+
+	let params = alchemy.createSchema();
+	params.addField('name', 'String');
+	params.addField('value', 'String');
+
+	this.schema.addField('parameters', params, {array: true});
+});
+
+/**
+ * Populate a link widget
+ *
+ * @author   Jelle De Loecker   <jelle@elevenways.be>
+ * @since    0.6.2
+ * @version  0.6.2
+ *
+ * @param    {HTMLAnchorElement}   anchor
+ * @param    {Alchemy.Widget.Link} widget
+ */
+RouteLink.setMethod(function populateWidget(anchor, widget) {
+
+	let parameters = {};
+
+	if (Array.isArray(this.config.parameters)) {
+		let entry;
+
+		for (entry of this.config.parameters) {
+			parameters[entry.name] = entry.value;
+		}
+	}
+
+	this.hawkejs_renderer.helpers.Router.applyDirective(anchor, this.config.route, {
+		parameters : parameters,
+	});
+	
+});
